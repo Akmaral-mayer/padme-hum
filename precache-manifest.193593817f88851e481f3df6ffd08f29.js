ad6b2a2f1071cb650dce82c0e2a314c188d0e4ad
Islamova Akmaral <akmaralslmv@gmail.com>
2019-11-24 00:24:23 +0600

    Updates

---
+++ precache-manifest.193593817f88851e481f3df6ffd08f29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77543226ff20c64614712ebc204501f1",
+    "url": "/padme-hum/index.html"
+  },
+  {
+    "revision": "bf6e528b35d89f3a1ff2",
+    "url": "/padme-hum/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6199b3162f1edfb7da8d",
+    "url": "/padme-hum/static/js/2.9680e009.chunk.js"
+  },
+  {
+    "revision": "bf6e528b35d89f3a1ff2",
+    "url": "/padme-hum/static/js/main.3826c0f3.chunk.js"
+  },
+  {
+    "revision": "e74b51779c2d49a93a4a",
+    "url": "/padme-hum/static/js/runtime-main.8cbabe0c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/padme-hum/static/media/logo.25bf045c.svg"
+  }
+]);
